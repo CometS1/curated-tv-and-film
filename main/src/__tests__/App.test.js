67166b39d5035fe2e7ea1768d2ca137e22d54b26
Shawn <sppang1@yahoo.com>
2018-12-10 11:06:15 -0500

    Added test for toggle filter method

--- main/src/__tests__/App.test.js
+++ main/src/__tests__/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import render  from 'react-test-renderer';
+import render from 'react-test-renderer';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -10,7 +14,14 @@ it('renders without crashing', () => {
 });
 
 it('snapshot changes when input change', () => {
-    const component = render.create(<App/>);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  const component = render.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('check for toggling state change', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state().showFilters).toEqual(false);
+  wrapper.instance().toggleFilter();
+  expect(wrapper.state().showFilters).toEqual(true);
 });
